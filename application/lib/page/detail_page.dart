7dd64ebbed3372562dbc41a1f28715b341c43a3d
Arisa Asa <aumnoiarisa@gmail.com>
2025-04-18 23:17:18 +0700

    add detail page

---
+++ application/lib/page/detail_page.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  int quantity = 1;
+
+  void increaseQty() {
+    setState(() {
+      quantity++;
+    });
+  }
+
+  void decreaseQty() {
+    if (quantity > 1) {
+      setState(() {
+        quantity--;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: Icon(Icons.arrow_back, color: Color(0xFF3C40C6), size: 32),
+          title: const Text(
+            'รายละเอียดสินค้า',
+            style: TextStyle(
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              Container(
+                margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                width: 240,
+                height: 240,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      offset: Offset(4, 6),
+                      blurRadius: 12,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Icon(Icons.image, size: 100, color: Colors.grey),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 363,
+                decoration: BoxDecoration(
+                  color: Color(0xFFE9E9E9),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      offset: Offset(4, 6),
+                      blurRadius: 12,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, right: 12),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          'รหัสสินค้า : 10001003',
+                          style: TextStyle(fontSize: 12, color: Colors.black54),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(8),
+                      color: Color(0xFF3C40C6),
+                      child: Text(
+                        'ชื่อ :  ปูนงานโครงสร้าง SCG สูตรไฮบริด \n 50 กก.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          height: 1.4,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'ประเภท : ปูนซีเมนต์',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          Text(
+                            'ยี่ห้อ : SCG',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on, size: 20),
+                              SizedBox(width: 6),
+                              Text(
+                                'Location : A001',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              'ราคา 150฿ / ถุง',
+                              style: TextStyle(
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'จำนวน',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 10),
+                    // ปุ่มลบ
+                    GestureDetector(
+                      onTap: decreaseQty,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[300],
+                        child: Icon(Icons.remove, color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      '$quantity',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 10),
+                    GestureDetector(
+                      onTap: increaseQty,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[300],
+                        child: Icon(Icons.add, color: Colors.black),
+                      ),
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent[400],
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        elevation: 4,
+                      ),
+                      child: Text(
+                        'เพิ่มลงในรถเข็น',
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
